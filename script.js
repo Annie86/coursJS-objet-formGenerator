0b2e9b2cd30abe2cde551662c99bd07bc5e57d6c
Unknown <niamor@zliton.com>
2017-11-23 10:00:50 +0100

    add jQuery + change form name

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 	// javascript objects for the form, questions and answers
 
 function form() {
-	this.name;
+	this.name = 'my first form';
 	this.questions = [];
 }
 
@@ -16,28 +16,22 @@ function answer() {
 	this.text;
 }
 
-	// test the objects with hardcoded instances
+var myForm = new form();
 
-var form1 = new form();
-form1.name = "formulaire 1";
+function changeFormName(){
+	myForm.name = $(this).val();
+}
 
-var question1 = new question();
-question1.text = 'ça va ?';
-question1.type = 'radio';
+function init() {
 
-var answer1 = new answer();
-answer1.text = 'oui';
-question1.answers.push(answer1);
-var answer2 = new answer();
-answer2.text = 'non';
-question1.answers.push(answer2);
-form1.questions.push(question1);
+	$('#debug').click(function() {
+		console.log(myForm);
+	});
 
-var question2 = new question();
-question2.text = 'oui ou non ?';
-question2.type = 'checkbox';
-question2.answers.push(answer1);
-question2.answers.push(answer2);
-form1.questions.push(question2);
+	$('#formName')
+		.keyup(changeFormName)
+		.val(myForm.name);
 
-console.log(form1);
+}
+
+$(init);
