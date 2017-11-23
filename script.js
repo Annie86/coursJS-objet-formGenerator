2cffcf18634298e01c4860339667929159ea6b26
Unknown <niamor@zliton.com>
2017-11-23 09:44:34 +0100

    js objects for forms, questions, answers  + hardcoded tests

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 
-function question() {
+	// javascript objects for the form, questions and answers
 
+function form() {
+	this.name;
+	this.questions = [];
 }
 
-var question1 = new question();
+function question() {
+	this.text;
+	this.type;
+	this.answers = [];
+}
 
+function answer() {
+	this.text;
+}
+
+	// test the objects with hardcoded instances
+
+var form1 = new form();
+form1.name = "formulaire 1";
+
+var question1 = new question();
+question1.text = 'ça va ?';
+question1.type = 'radio';
+
+var answer1 = new answer();
+answer1.text = 'oui';
+question1.answers.push(answer1);
+var answer2 = new answer();
+answer2.text = 'non';
+question1.answers.push(answer2);
+form1.questions.push(question1);
+
+var question2 = new question();
+question2.text = 'oui ou non ?';
+question2.type = 'checkbox';
+question2.answers.push(answer1);
+question2.answers.push(answer2);
+form1.questions.push(question2);
+
+console.log(form1);
